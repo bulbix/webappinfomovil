8f299c92994048b2f7149e7781feef6e6dc6e178
Luis Prado <lfpradof@gmail.com>
2015-07-08 10:51:50 -0500

    login

--- src/main/java/com/infomovil/webapp/HomeController.java
+++ src/main/java/com/infomovil/webapp/HomeController.java
@@ -1,16 +1,23 @@
 package com.infomovil.webapp;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.infomovil.webapp.util.Util;
 
@@ -39,9 +46,19 @@ public class HomeController {
 		logger.info("password " + auth.getCredentials().toString());
 		
 		model.addAttribute("usuario", auth.getName().toString());
-		model.addAttribute("pasword", auth.getCredentials().toString());
+		model.addAttribute("password", auth.getCredentials().toString());
 		model.addAttribute("serverTime", formattedDate );
 		
+		return "home";
+	}
+	
+	@RequestMapping(value = "/home/luis", method = RequestMethod.GET)
+	
+	public String luis(Model model) {
+		Util.loginUsuario("luis@mail.com", "garbage1");
+		Authentication auth = Util.getUserLogged();
+		model.addAttribute("usuario", auth.getName().toString());
+		model.addAttribute("password", auth.getCredentials().toString());
 		return "home";
 	}
 	
--- src/main/java/com/infomovil/webapp/util/Util.java
+++ src/main/java/com/infomovil/webapp/util/Util.java
@@ -1,8 +1,14 @@
 package com.infomovil.webapp.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -26,6 +32,22 @@ public class Util {
 		}
 		
 	}	
+	
+	/**
+	 * Loguea el usuario en spring security
+	 * @param usuario
+	 * @param password
+	 */
+	public static void loginUsuario(String usuario, String password){
+		
+		//getCurrentSession().invalidate();
+		
+		List<GrantedAuthority> grantedAuths = new ArrayList<GrantedAuthority>();
+		grantedAuths.add(new SimpleGrantedAuthority("ROLE_USER"));
+		
+		Authentication auth1 = new UsernamePasswordAuthenticationToken (usuario, password, grantedAuths);
+		SecurityContextHolder.getContext().setAuthentication(auth1);
+	}
 
 	/****
 	 * 
