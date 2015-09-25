3916cc17b55a62a54aac74de95b8d096c4f7f038
Luis Prado <lfpradof@gmail.com>
2015-09-25 10:41:34 -0500

    espacio

--- src/main/java/com/infomovil/webapp/controllers/WebappController.java
+++ src/main/java/com/infomovil/webapp/controllers/WebappController.java
@@ -1,8 +1,6 @@
 
 package com.infomovil.webapp.controllers;
 
-import static org.junit.Assert.assertNotNull;
-
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +16,6 @@ import org.apache.commons.validator.routines.EmailValidator;
 import org.apache.log4j.Logger;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -463,7 +460,7 @@ public class WebappController
 		}
 		
 	}
-
+	
 	private ModelAndView validaURL(String vista)
 	{
 		HashMap<String, Object> model = new HashMap<String, Object>();		
@@ -472,70 +469,18 @@ public class WebappController
 		return new ModelAndView(vista, model);
 	}
 	
-	@RequestMapping(value = "/infomovil/miCuenta", method = {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView miCuenta(HttpServletRequest request, HttpServletResponse response, RedirectAttributes redirectAttributes, String payment_status)
-	{		
-		HashMap<String, Object> model = new HashMap<String, Object>();
-		RespuestaVO wsRespuesta = new RespuestaVO();
-		
-		String claseProductos = "'col-xs-12 col-sm-6 col-md-6 col-lg-6 dBlock'";
-		String claseCss = "";
-		String colorTexto = "";
-		String extensionImg = "";
-		int totProductos = 0;
-
-		try
-		{		
-			String correo = Util.getUserLogged().getUsername();
-			String password = Util.getUserLogged().getPassword();
-			
-			wsRespuesta = wsCliente.crearSitioGetProductosUsuario(correo, password);
-			totProductos = wsRespuesta.getListProductoUsuarioVO().size();
-			
-			if (totProductos == 1)
-				claseProductos = "'col-xs-12 col-sm-6 col-md-6 col-lg-6 dBlock col-sm-offset-3'";
-		
-			if (Util.getCurrentSession().getAttribute("canal").toString().startsWith("BAZ"))
-			{
-				claseCss = "default";
-				colorTexto = "textBlack";
-				extensionImg = "-bk";
-			}
-			else
-			{
-				claseCss = "inverse";
-				colorTexto = "textWhite";
-				extensionImg = "";
-			}
-			
-			model.put("claseProductos", claseProductos);
-			model.put("claseCss", claseCss);
-			model.put("colorTexto", colorTexto);
-			model.put("extensionImg", extensionImg);
-			model.put("totProductos", totProductos);
-			model.put("productos", wsRespuesta.getListProductoUsuarioVO());
-			model.put("correoElectronico", correo);
-			model.put("paymentStatus", payment_status);
-			
-		}		
-		catch (Exception e) 
-		{
-			logger.error("miCuenta:::::", e);
-			return null;
-		}			
-		return new ModelAndView("Webapp/miCta", model);
-	}
-
 	@RequestMapping(value = "/infomovil/editarSitio", method = RequestMethod.GET)
 	public ModelAndView editarSitio(HttpServletRequest request, HttpServletResponse response, RedirectAttributes redirectAttributes)
 	{		
 		HashMap<String, Object> model = new HashMap<String, Object>();
 		RespuestaVO wsRespuesta = new RespuestaVO();
+		StatusDomainVO statusDom = new StatusDomainVO();
 		
 	    String template = "Coverpage1azul";
 		String sitioWeb = "SIN_PUBLICAR";
 		String canal = "NO_TIENE";
 		String claseCss = "default";
+		String campania = "basica";
 		String colorTexto = "textWhite";
 		String extensionImg = "";
 		String fechaIni = "";
@@ -557,11 +502,12 @@ public class WebappController
 			String password = Util.getUserLogged().getPassword();
 
 		    wsRespuesta = wsCliente.crearSitioCargar(correo, password);
-		    
+			
 			if (wsRespuesta.getCodeError().equals("0"))
 			{
 				Util.getCurrentSession().setAttribute("nombreUsuario", 
-				wsRespuesta.getDominioCreaSitio().getNombreUsuario());	
+				wsRespuesta.getDominioCreaSitio().getNombreUsuario());				
+				campania = wsRespuesta.getDominioCreaSitio().getCampania().toLowerCase();
 				idDominio = wsRespuesta.getIdDominio();
 				downgrade = wsRespuesta.getDowngrade();
 				
@@ -595,6 +541,8 @@ public class WebappController
 					fechaIni = wsRespuesta.getFTelNamesIni();
 					fechaFin = wsRespuesta.getFTelNamesFin();
 				}
+
+				/*Ajuste para trabajar con la lista de productos*/
 
 				if (wsRespuesta.getDominioCreaSitio().getCanal().startsWith("BAZ"))
 				{
@@ -634,7 +582,7 @@ public class WebappController
 					ProductoUsuarioVO productoVO = null;
 					productoVO = modeloWebApp.getProducto("tel");
 					
-					if (productoVO != null) 
+					if (productoVO != null) /*Tipo de dominio a publicar*/
 					{
 						tipoPublica = "tel";
 						visibleTel = "display:block;";
@@ -646,17 +594,19 @@ public class WebappController
 					productoVO = null;
 					productoVO = modeloWebApp.getProducto("pp", "pi");
 					
-					if (productoVO != null)
+					if (productoVO != null) /*Busca si en los productos tiene un plan pro*/
 						planPro = "SI";
 				}
-			
+
+				/*Ajuste para validar si este usuario tiene un PP (comprado)*/
 				if (planPro.equals("NO"))
 				{
 					status = wsRespuesta.getDominioCreaSitio().getEstatusCuenta();
 					if (modeloWebApp.getStatus(status))
 						planPro = "SI";
 				}
-							
+				/*End ajuste para trabajar con la lista de productos*/
+				
 				model.put("usuarioLogueado", correo);
 				model.put("nombreUsuario", wsRespuesta.getDominioCreaSitio().getNombreUsuario().trim());				
 				model.put("nombreEmpresa", wsRespuesta.getDominioCreaSitio().getNombreEmpresa().trim());
@@ -685,8 +635,6 @@ public class WebappController
 				model.put("idDominio", idDominio);
 				model.put("downgrade", downgrade);
 				model.put("galeriaImagenes", galeriaImagenes);
-				
-			    Util.getCurrentSession().setAttribute("canal", canal);	
 			}
 			else 
 			{
@@ -702,6 +650,196 @@ public class WebappController
 			}
 
 			return new ModelAndView("Webapp/editorSitio", model);
+		}		
+		catch (Exception e) 
+		{
+			logger.error("cargarInformacion:::::", e);
+			return null;
+		}	
+	}
+	
+	
+	@RequestMapping(value = "/infomovil/miCuenta", method = RequestMethod.GET)
+	public ModelAndView miCuenta(HttpServletRequest request, HttpServletResponse response, RedirectAttributes redirectAttributes)
+	{		
+		HashMap<String, Object> model = new HashMap<String, Object>();
+		RespuestaVO wsRespuesta = new RespuestaVO();
+		StatusDomainVO statusDom = new StatusDomainVO();
+		
+	    String template = "Coverpage1azul";
+		String sitioWeb = "SIN_PUBLICAR";
+		String canal = "NO_TIENE";
+		String claseCss = "default";
+		String campania = "basica";
+		String colorTexto = "textWhite";
+		String extensionImg = "";
+		String fechaIni = "";
+		String fechaFin = "";
+		String status = "";
+		String esquemaProducto = "";
+		String tipoPublica = "";
+		String planPro = "";
+		String urlEjemploSitio = "";
+		String visibleRecurso = "";
+		String visibleTel = "";
+		String idDominio = "";
+		String downgrade = "";
+		String galeriaImagenes = "";
+		
+		try
+		{
+			String correo = Util.getUserLogged().getUsername();
+			String password = Util.getUserLogged().getPassword();
+
+		    wsRespuesta = wsCliente.crearSitioCargar(correo, password);
+			
+			if (wsRespuesta.getCodeError().equals("0"))
+			{
+				Util.getCurrentSession().setAttribute("nombreUsuario", 
+				wsRespuesta.getDominioCreaSitio().getNombreUsuario());				
+				campania = wsRespuesta.getDominioCreaSitio().getCampania().toLowerCase();
+				idDominio = wsRespuesta.getIdDominio();
+				downgrade = wsRespuesta.getDowngrade();
+				
+				for (StatusDomainVO stat : wsRespuesta.getListStatusDomainGratisVO())
+				{
+					if (stat.getDescripcionItem().equalsIgnoreCase("GALERIA DE IMAGENES"))
+					{
+						galeriaImagenes = stat.getStatus();
+						break;
+					}
+				}
+				
+				if (wsRespuesta.getDominioCreaSitio().getNombreEmpresa().trim().equals("TÃtulo"))
+					model.put("nombreEmpresa", "");
+				
+				if (!StringUtils.isEmpty(wsRespuesta.getDominioCreaSitio().getSitioWeb()))
+					sitioWeb = wsRespuesta.getDominioCreaSitio().getSitioWeb();
+
+				if (wsRespuesta.getDominioCreaSitio().getSitioWeb().indexOf("@") != -1)
+					sitioWeb = "SIN_PUBLICAR";
+				
+				if (!StringUtils.isEmpty(wsRespuesta.getDominioCreaSitio().getTemplate()))
+					template = wsRespuesta.getDominioCreaSitio().getTemplate();
+
+				if (template.equals("Moderno") || template.equals("Creativo") || template.equals("Clasico") 
+						|| template.equals("Divertido") || template.equals("Estandar1"))
+					template = "Coverpage1azul";
+				
+				if (sitioWeb.indexOf("tel") != -1)
+				{
+					fechaIni = wsRespuesta.getFTelNamesIni();
+					fechaFin = wsRespuesta.getFTelNamesFin();
+				}
+
+				/*Ajuste para trabajar con la lista de productos*/
+
+				if (wsRespuesta.getDominioCreaSitio().getCanal().startsWith("BAZ"))
+				{
+					canal = "BAZ";
+					claseCss = "default";
+					colorTexto = "textBlack";
+					extensionImg = "-bk";
+					tipoPublica = "tel";
+					planPro = "SI";
+					visibleTel = "display:block;";
+					visibleRecurso = "display:none";
+					urlEjemploSitio = "www.misitio.tel";
+				}
+				else
+				{
+					model.put("dominios", obtenerDominios());
+					canal = wsRespuesta.getDominioCreaSitio().getCanal();
+					claseCss = "inverse";
+					colorTexto = "textWhite";
+					extensionImg = "";
+					tipoPublica = "recurso";
+					visibleRecurso = "display:block;";
+					visibleTel = "display:none";
+					urlEjemploSitio = "";
+					planPro = "NO";
+				}
+				
+				esquemaProducto = wsRespuesta.getEsquemaProducto();
+				modeloWebApp.setListaProductos(wsRespuesta.getListProductoUsuarioVO());
+				
+				if (esquemaProducto.equals("NEW"))
+				{
+					tipoPublica = "recurso";
+					visibleRecurso = "display:block;";
+					visibleTel = "display:none";
+					urlEjemploSitio = "";
+					ProductoUsuarioVO productoVO = null;
+					productoVO = modeloWebApp.getProducto("tel");
+					
+					if (productoVO != null) /*Tipo de dominio a publicar*/
+					{
+						tipoPublica = "tel";
+						visibleTel = "display:block;";
+						visibleRecurso = "display:none";
+						urlEjemploSitio = "www.misitio.tel";
+					}
+					
+					planPro = "NO";
+					productoVO = null;
+					productoVO = modeloWebApp.getProducto("pp", "pi");
+					
+					if (productoVO != null) /*Busca si en los productos tiene un plan pro*/
+						planPro = "SI";
+				}
+
+				/*Ajuste para validar si este usuario tiene un PP (comprado)*/
+				if (planPro.equals("NO"))
+				{
+					status = wsRespuesta.getDominioCreaSitio().getEstatusCuenta();
+					if (modeloWebApp.getStatus(status))
+						planPro = "SI";
+				}
+				/*End ajuste para trabajar con la lista de productos*/
+				
+				model.put("usuarioLogueado", correo);
+				model.put("nombreUsuario", wsRespuesta.getDominioCreaSitio().getNombreUsuario().trim());				
+				model.put("nombreEmpresa", wsRespuesta.getDominioCreaSitio().getNombreEmpresa().trim());
+				model.put("descripcionCorta", wsRespuesta.getDominioCreaSitio().getDescripcionCorta().trim());
+				model.put("correoElectronico", wsRespuesta.getDominioCreaSitio().getCorreoElectronico().trim());
+				model.put("telefonoUsuario", wsRespuesta.getDominioCreaSitio().getTelefono().trim());			
+				model.put("vistaPrevia", wsRespuesta.getDominioCreaSitio().getUrlVistaPrevia());	
+				model.put("urlVideo", wsRespuesta.getDominioCreaSitio().getVideoUrl());
+				model.put("latitud", wsRespuesta.getDominioCreaSitio().getLatitudeMap());
+				model.put("longitud", wsRespuesta.getDominioCreaSitio().getLongitudeMap());
+				model.put("direccionMap", wsRespuesta.getDominioCreaSitio().getDireccionMap());
+				model.put("statusCuenta", wsRespuesta.getDominioCreaSitio().getTipoCuenta().toLowerCase());
+				model.put("template", template);
+				model.put("sitioWeb", sitioWeb); 
+				model.put("fechaIniTel", fechaIni);
+				model.put("fechaFinTel", fechaFin);
+				model.put("canalUsuario", canal);
+				model.put("claseCss", claseCss);
+				model.put("colorTexto", colorTexto);
+				model.put("extensionImg", extensionImg);		
+				model.put("tipoPublica", tipoPublica);
+				model.put("planPro", planPro);
+				model.put("urlEjemploSitio", urlEjemploSitio);
+				model.put("visibleRecurso", visibleRecurso);
+				model.put("visibleTel", visibleTel);
+				model.put("idDominio", idDominio);
+				model.put("downgrade", downgrade);
+				model.put("galeriaImagenes", galeriaImagenes);
+			}
+			else 
+			{
+				logoutInfomovil(request, response);
+				
+				if (wsRespuesta.getCodeError().equals("-3")){
+					redirectAttributes.addFlashAttribute("errorCta", "Si ya tienes Plan Pro. Inicia sesión");
+					redirectAttributes.addFlashAttribute("ctaCorreo", correo);
+				}
+				
+				ModelAndView modelAndView =  new ModelAndView("redirect:/login");
+				return modelAndView;
+			}
+
+			return new ModelAndView("Webapp/miCuenta", model);
 		}		
 		catch (Exception e) 
 		{
@@ -775,54 +913,6 @@ public class WebappController
 		
 		return resultMap;
 	}
-	
-	
-	@RequestMapping(value = "/infomovil/crearSitioIntentoPago", method = RequestMethod.POST, produces = "application/json")
-	@ResponseBody
-	public Map<String, String> crearSitioIntentoPago(@RequestParam String nombre, @RequestParam String direccion,@RequestParam String pais)
-	{		
-		Map<String, String> resultMap = new HashMap<String, String>();
-		RespuestaVO wsRespuesta = new RespuestaVO();
-		String correo = Util.getUserLogged().getUsername();
-		String password = Util.getUserLogged().getPassword();
-		try
-		{
-			wsRespuesta = wsCliente.crearSitioIntentoPago(correo, password, "DOMINIO TEL", "PAY PAL", "TEL", "DOMINIO TEL",nombre,direccion,pais);
-				
-			resultMap.put("resultado", wsRespuesta.getIdPago());
-		}		
-		catch (Exception e) 
-		{
-			logger.error("existeDominio:::::", e);
-		}	
-		
-		return resultMap;
-	}
-	
-	@RequestMapping(value = "/infomovil/getProductosUsuario", method = RequestMethod.POST, produces = "application/json")
-	@ResponseBody
-	public Map<String, String> getProductosUsuario(@RequestParam String nombreDominio, @RequestParam String tipoDominio)
-	{		
-		Map<String, String> resultMap = new HashMap<String, String>();
-		RespuestaVO wsRespuesta = new RespuestaVO();
-		
-		try
-		{
-			wsRespuesta = wsCliente.crearSitioGetProductosUsuario("rambo1@mail.com", "garbage1");
-			//wsRespuesta = wsCliente.crearSitioIntentoPago(nombreDominio, tipoDominio);
-			
-			System.out.println("Tamanio de lista " + wsRespuesta.getListProductoUsuarioVO().size());
-			resultMap.put("resultado", wsRespuesta.getResultado());
-		}		
-		catch (Exception e) 
-		{
-			logger.error("existeDominio:::::", e);
-		}	
-		
-		return resultMap;
-	}
-	
-	
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(){
