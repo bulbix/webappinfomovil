e40cb4b7c97f13ec4ed4a1c84500865dcb631f22
Luis Prado <lfpradof@gmail.com>
2015-09-28 18:38:06 -0500

    metodos promociones

---
+++ src/main/java/com/infomovil/webapp/clientWsInfomovil/CrearSitioGuardarPromocion.java
@@ -0,0 +1,278 @@
+
+package com.infomovil.webapp.clientWsInfomovil;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para crearSitio_guardarPromocion complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="crearSitio_guardarPromocion">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="email" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="password" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="descripcion" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="fechaVigencia" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="redimir" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="terminos" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="titulo" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="base64Imagen" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="idPromocion" type="{http://www.w3.org/2001/XMLSchema}int" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "crearSitio_guardarPromocion", propOrder = {
+    "email",
+    "password",
+    "descripcion",
+    "fechaVigencia",
+    "redimir",
+    "terminos",
+    "titulo",
+    "base64Imagen",
+    "idPromocion"
+})
+public class CrearSitioGuardarPromocion {
+
+    @XmlElement(required = true)
+    protected String email;
+    protected String password;
+    protected String descripcion;
+    protected String fechaVigencia;
+    protected String redimir;
+    protected String terminos;
+    protected String titulo;
+    protected String base64Imagen;
+    protected Integer idPromocion;
+
+    /**
+     * Obtiene el valor de la propiedad email.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Define el valor de la propiedad email.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEmail(String value) {
+        this.email = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad password.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * Define el valor de la propiedad password.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPassword(String value) {
+        this.password = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad descripcion.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * Define el valor de la propiedad descripcion.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDescripcion(String value) {
+        this.descripcion = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad fechaVigencia.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getFechaVigencia() {
+        return fechaVigencia;
+    }
+
+    /**
+     * Define el valor de la propiedad fechaVigencia.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setFechaVigencia(String value) {
+        this.fechaVigencia = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad redimir.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getRedimir() {
+        return redimir;
+    }
+
+    /**
+     * Define el valor de la propiedad redimir.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setRedimir(String value) {
+        this.redimir = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad terminos.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTerminos() {
+        return terminos;
+    }
+
+    /**
+     * Define el valor de la propiedad terminos.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTerminos(String value) {
+        this.terminos = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad titulo.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * Define el valor de la propiedad titulo.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTitulo(String value) {
+        this.titulo = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad base64Imagen.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBase64Imagen() {
+        return base64Imagen;
+    }
+
+    /**
+     * Define el valor de la propiedad base64Imagen.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBase64Imagen(String value) {
+        this.base64Imagen = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad idPromocion.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Integer }
+     *     
+     */
+    public Integer getIdPromocion() {
+        return idPromocion;
+    }
+
+    /**
+     * Define el valor de la propiedad idPromocion.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Integer }
+     *     
+     */
+    public void setIdPromocion(Integer value) {
+        this.idPromocion = value;
+    }
+
+}
