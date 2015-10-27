1472a97dc08d868812b0071aa2168f4a9fe5d2e0
Elizabeth Jazmin Basurto Millan <biblos01@gmail.com>
2015-10-27 13:55:49 -0600

    Template 10 y 11, promo 4

--- src/main/webapp/resources/js/webapp/validaciones.js
+++ src/main/webapp/resources/js/webapp/validaciones.js
@@ -1,5 +1,5 @@
-var templates = new Array("Coverpage7", "Coverpage8", "Coverpage1azul", "Coverpage2", "Coverpage3", "Coverpage4", "Coverpage5", "Coverpage6");
-var nombres = new Array("Otoño", "Vanguardia", "Portada azul", "Restaurantes", "Belleza", "Reposter&iacute;as", "Automotriz", "Taxistas"); /*Cambiar nombres*/
+var templates = new Array("Coverpage11", "Coverpage10", "Coverpage9", "Coverpage7", "Coverpage8", "Coverpage1azul", "Coverpage2", "Coverpage3", "Coverpage4", "Coverpage5", "Coverpage6");
+var nombres = new Array("Metrópoli", "Nuevos negocios", "Pr&oacute;ximamente", "Otoño", "Vanguardia", "Portada azul", "Restaurantes", "Belleza", "Reposter&iacute;as", "Automotriz", "Taxistas"); /*Cambiar nombres*/
 var nombreDominio = "";
 var tipoDominio = "";
 var indice = 0;
