38a6ae9af406df7642f267569fc277b9745f0341
Elizabeth Jazmin Basurto Millan <biblos01@gmail.com>
2015-11-05 10:27:31 -0600

    Nuevos templates

--- src/main/webapp/resources/js/webapp/validaciones.js
+++ src/main/webapp/resources/js/webapp/validaciones.js
@@ -1,5 +1,5 @@
-var templates = new Array("Coverpage11", "Coverpage10", "Coverpage9", "Coverpage7", "Coverpage8", "Coverpage1azul", "Coverpage2", "Coverpage3", "Coverpage4", "Coverpage5", "Coverpage6");
-var nombres = new Array("Metrópoli", "Nuevos negocios", "Pr&oacute;ximamente", "Otoño", "Vanguardia", "Portada azul", "Restaurantes", "Belleza", "Reposter&iacute;as", "Automotriz", "Taxistas"); /*Cambiar nombres*/
+var templates = new Array("Coverpage20","Coverpage19","Coverpage18","Coverpage17","Coverpage16","Coverpage15","Coverpage14","Coverpage13","Coverpage12","Coverpage11", "Coverpage10", "Coverpage9", "Coverpage7", "Coverpage8", "Coverpage1azul", "Coverpage2", "Coverpage3", "Coverpage4", "Coverpage5", "Coverpage6");
+var nombres = new Array("20","19","18","17","16","15","14","13","12","Metrópoli", "Nuevos negocios", "Pr&oacute;ximamente", "Otoño", "Vanguardia", "Portada azul", "Restaurantes", "Belleza", "Reposter&iacute;as", "Automotriz", "Taxistas"); /*Cambiar nombres*/
 var nombreDominio = "";
 var tipoDominio = "";
 var indice = 0;
